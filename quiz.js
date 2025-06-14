76277ab247770c3f2f28b395365e3df343219d3f
Wangui Githua <barb.wangui@gmail.com>
2025-06-14 19:09:04 -0400

    Compare answers

--- quiz.js
+++ quiz.js
@@ -2,6 +2,15 @@
 
 function checkAnswer(){
     const correctAnswer = "4";
-    const selected = document.querySelector('input[name="quiz" :checked')
-    const userAnswer = selected.value ;
+    const selectedAnswer = document.querySelector('input[name="quiz"]:checked')
+    const userAnswer = selectedAnswer.value ;
+    const feedbackElement = document.getElementById('feedback');
+
+    // compare answers
+
+    if(userAnswer === correctAnswer){
+        feedbackElement.textContent = "Correct! Well done."
+    } else {
+        feedbackElement.textContent = "That's incorrect. Try again!"
+    }
 }
